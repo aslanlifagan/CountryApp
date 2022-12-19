91f342d995f5214ea8fbad789209394a1f7531eb
aslanlifagan <aslanli9696@gmail.com>
2022-12-19 20:30:30 +0400

    Initial Commit

---
+++ CountryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CountryApp
+//
+//  Created by Fagan Aslanli on 19.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
